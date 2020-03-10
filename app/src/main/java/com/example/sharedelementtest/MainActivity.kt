61753cd612376ec9b29981f74c307ae209e4699c
TS01923141 <z890709@yahoo.com.tw>
2020-03-10 15:18:37 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/sharedelementtest/MainActivity.kt
+++ app/src/main/java/com/example/sharedelementtest/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
 //        postponeEnterTransition()
 
         imageView_small.setOnClickListener {
-            window.enterTransition
             var intent = Intent(this, DetailActivity::class.java)
             var options = ActivityOptions.makeSceneTransitionAnimation(
                 this,
